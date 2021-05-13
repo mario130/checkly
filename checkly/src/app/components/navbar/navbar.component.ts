da5818fac12ed460c0182a12b493f993e16edbaf
mario130 <mario.yonan.work@gmail.com>
2021-05-14 00:56:54 +0200

    add mobile navbar

--- checkly/src/app/components/navbar/navbar.component.ts
+++ checkly/src/app/components/navbar/navbar.component.ts
@@ -12,4 +12,9 @@ export class NavbarComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  isMobileNavOpen = false
+
+  switchMobileNavState() {
+    this.isMobileNavOpen = !this.isMobileNavOpen;
+  }
 }
--- checkly/tailwind.config.js
+++ checkly/tailwind.config.js
@@ -2,10 +2,25 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#023047",
+        "pr-medium": "#219EBC",
+        "pr-light": "#8ECAE6",
+
+        secondary: "#FFB703",
+        "sec-orange": "#FB8500",
+      },
+      fontFamily: {
+        logo: "Courgette",
+        two: "Merriweather",
+        one: "Noto Sans",
+        three: "Roboto",
+      },
+    },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
