949f28e5349138c4d46827c5c8877baedd099f2c
mario130 <mario.yonan.work@gmail.com>
2021-05-17 19:44:45 +0200

    using one main list + add homepage background

--- checkly/src/app/pages/homepage/homepage.component.ts
+++ checkly/src/app/pages/homepage/homepage.component.ts
@@ -9,13 +9,15 @@ import { ListsService } from './lists.service';
 })
 export class HomepageComponent implements OnInit {
   listsResponse!: IListsResponse;
+  mainList!: { name: string }[]
 
   constructor(private ListsService: ListsService) { }
 
   ngOnInit(): void {
     this.ListsService.listsFetched.subscribe((response: any) => {
       this.listsResponse = response
-      console.log(response)
+      // console.log(response)
+      this.mainList = response.lists[0].content
     })
     this.ListsService.getLists()
   }
